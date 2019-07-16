5bcb32a627691d43ab6daef0ea748441e5724fdb
spezi77 <spezi7713@gmx.net>
2019-07-16 20:47:33 +0200

    Smart Charging: Make it optional -- Use fwb boolean
    Change-Id: I0dcbdab57e5cef2e599054f570137078a751d4ac

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -234,6 +234,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
+
+        // Check availability of Smart Charging
+        Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_smartChargingAvailable)) {
+            getPreferenceScreen().removePreference(mSmartCharging);
+        }
     }
 
     @Override
